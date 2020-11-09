b42154c46986f03c019cbf57aef89562a40700df
Oleksandr Poliarush <oleksandr@Oleksandrs-MacBook-Pro.local>
2020-11-09 21:07:06 +0200

    add init sql

--- internal/env/new-db.go
+++ internal/env/new-db.go
@@ -31,20 +31,3 @@ func NewDb(c DbConfig) (*sql.DB, error) {
 
 	return db, nil
 }
-
-// where to store it?should I store it?
-func createTables(db *sql.DB) error {
-	script := `
-		create table users ( user_id SERIAL primary key, name varchar(100) not null );
-
-		create table room ( room_id SERIAL primary key, name varchar(50) not null, private boolean );
-		
-		create table participant ( participant_id SERIAL primary key, user_id int references users (user_id), room_id int references room (room_id) );
-		
-		create table message ( message_id SERIAL primary key, user_id int references users (user_id), room_id int references room (room_id), message varchar(500) );
-	`
-
-	_, err := db.Exec(script)
-
-	return err
-}
